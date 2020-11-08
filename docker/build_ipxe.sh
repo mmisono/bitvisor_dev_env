fa0f2bd5731941fa93019fb7d89270d64d6fba0a
Masanori Misono <m.misono760@gmail.com>
2020-11-08 12:32:03 +0900

    update script

--- docker/build_ipxe.sh
+++ docker/build_ipxe.sh
@@ -1,5 +1,5 @@
 cd /share/ipxe/src
 sed -i "s/^\/\/\(#define\tVMWARE\)/\1/" config/settings.h
-make bin/8086100f.mrom
+make bin/8086100f.mrom -j$(nproc)
 cp bin/8086100f.mrom /share/build
 git checkout .
